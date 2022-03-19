4cbedbbce43a78e6315371219169bd864a9239e0
T8080 <63805191+T8080@users.noreply.github.com>
2022-03-19 17:48:15 +0100

    move init to clojurescript

--- src/space/main.cljs
+++ src/space/main.cljs
@@ -1,6 +1,17 @@
 (ns space.main
-  [:require cljs.pprint space.parser])
+  [:require
+   cljs.pprint
+   space.parser])
 
 (defn parse [s]
   (with-out-str
     (cljs.pprint/pprint (space.parser/parse s))))
+
+(defn init []
+  (let [input (js/document.querySelector "#input")
+        button (js/document.querySelector "#button")
+        output (js/document.querySelector "#output")]
+    (set! (.-onclick button)
+          #(set! (.-innerHTML output)
+                 (parse (.-value input))))))
+
